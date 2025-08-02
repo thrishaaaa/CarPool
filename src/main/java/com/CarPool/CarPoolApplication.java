7b91a13e8038be5e3690c01e128249fa6e9c8ee8
thrishaaaa <thrishakiran14@gmail.com>
2025-08-02 17:59:58 +0530

    first commit

---
+++ src/main/java/com/CarPool/CarPoolApplication.java
@@ -0,0 +1,13 @@
+package com.CarPool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPoolApplication.class, args);
+	}
+
+}
